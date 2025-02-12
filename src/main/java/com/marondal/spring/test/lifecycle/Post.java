4142741cd3038025bf41c92c26d47bce7883e071
dulumary <lecture@hagulu.com>
2025-02-12 20:05:13 +0900

    [김인규] - 기본 Request, Response 문제 풀이

---
+++ src/main/java/com/marondal/spring/test/lifecycle/Post.java
@@ -0,0 +1,32 @@
+package com.marondal.spring.test.lifecycle;
+
+public class Post {
+	
+	private String title;
+	private String user;
+	private String content;
+	
+	public Post(String title, String user, String content) {
+		this.title = title;
+		this.user = user;
+		this.content = content;
+	}
+	
+	@Override
+	public String toString() {
+		return "제목 : " + title + "\n작성자 : " + user + "\n" + content; 
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
