c77d572af471044041d2ecf5efb5faef171846e1
dulumary <lecture@hagulu.com>
2025-02-12 21:49:51 +0900

    [김인규]  - 기본 request response 문제 풀이

---
+++ src/main/java/com/marondal/spring/test/database/repository/StoreRepository.java
@@ -0,0 +1,15 @@
+package com.marondal.spring.test.database.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.marondal.spring.test.database.domain.Store;
+
+@Mapper
+public interface StoreRepository {
+	
+	// store 테이블의 모든 행 조회 
+	public List<Store> selectStoreList();
+
+}
