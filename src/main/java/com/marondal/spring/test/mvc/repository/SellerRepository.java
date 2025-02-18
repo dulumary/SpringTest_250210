2b67665b24607f5fa8e86c9ae34d3e9e0bc86f81
dulumary <lecture@hagulu.com>
2025-02-18 19:48:45 +0900

    [김인규]  - mvc cycle 입력 문제 풀이

---
+++ src/main/java/com/marondal/spring/test/mvc/repository/SellerRepository.java
@@ -0,0 +1,13 @@
+package com.marondal.spring.test.mvc.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface SellerRepository {
+	
+	public int insertSeller(@Param("nickname") String nickname
+			, @Param("profileImage") String profileImage
+			, @Param("temperature") double temperature);
+
+}
---
+++ src/main/java/com/marondal/spring/test/mvc/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.marondal.spring.test.mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.test.mvc.repository.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	public int addSeller(String nickname
+			, String profileImage
+			, double temperature) {
+		
+		int count = sellerRepository.insertSeller(nickname, profileImage, temperature);
+		
+		return count;
+	}
+
+}
